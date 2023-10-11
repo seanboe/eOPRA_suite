3a3eff3406db1da3dab3ef0503941ca8f9eb3e54
Sean Boerhout <boeseany@gmail.com>
2023-10-11 14:25:39 -0400

    Serial problem solved

--- scripting/eOPRA_switcher_cli/main.py
+++ scripting/eOPRA_switcher_cli/main.py
@@ -1,35 +1,45 @@
 import click
 import serial
 import serial.tools.list_ports
-import time
 
-class eOPRA_Switcher(object):
-  def __init__(self):
-    self.comm_params = {}
-device = click.make_pass_decorator(eOPRA_Switcher, ensure=True)
+from src.utilities import USBTalk
+
+usb_device = click.make_pass_decorator(USBTalk, ensure=True)
 
 @click.group(invoke_without_command=True)
 @click.option('--port', '-p', default='', help='Manually select the device communications port')
 @click.option('--baud_rate', '-b', default=112500, help='This is the serial baud rate')
-@device
-def cli(device, port, baud_rate):
+@usb_device
+def cli(usb_device, port, baud_rate):
 
   if port:
-    device.comm_params['port'] = port
+    usb_device.port = port
   else:
     for port in list(serial.tools.list_ports.comports()):
-      if "__board__ name" in port.description:
-        device.comm_params['port'] = port.device
-        click.echo(f"Device found on port {device.port}")
+      if "Pico" in port.description:
+        usb_device.port = port.device
+        click.echo(f"Device found on port {usb_device.port}")
 
   if baud_rate:
-    device.comm_params['baud_rate'] = baud_rate
+    usb_device.baud_rate = baud_rate
 
   try:
-    device.comm_params['serial_device'] = serial.Serial(port=device.comm_parameters['port'], baudrate=device.comm_parameters['baudrate'], timeout=0.5)
+    usb_device.serial_device = serial.Serial(port=usb_device.port, baudrate=usb_device.baud_rate, timeout=usb_device.serial_timeout)
   except:
     click.echo("Error communicating to device. Try selecting another port or verifying the USB connection.")
+    exit()
+
   
+
+
+@cli.command()
+@usb_device
+def write_read(usb_device):
+  usb_device.write_read([0x05, 0x07])
+
+
+
+
 
 
 def read_config():
