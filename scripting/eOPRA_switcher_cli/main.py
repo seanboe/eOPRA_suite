dd5269c73b22cfae6fbe008142206c6b6cc9bc0b
Sean Boerhout <boeseany@gmail.com>
2023-10-11 15:46:58 -0400

    CLI cleanup updates

--- scripting/eOPRA_switcher_cli/main.py
+++ scripting/eOPRA_switcher_cli/main.py
@@ -33,11 +33,40 @@ def cli(usb_device, port, baud_rate):
 
 
 @cli.command()
+@click.option('--time', '-t', default='', help='Set the internal time')
+@click.option('--stim_on', '-p', default=1, help='Set stimulation on')
+@click.option('--stim_port', '-sp', default=8, help='Set the stimulation port')
+@click.option('--tft_on', '-tft', default=1, help='Set the tft on/off')
+@click.option('--broadcast_on', '-b', default=1, help='Set MQTT broadcasting on/off')
+@click.option('--debug_on', '-b', default=1, help='Set debugging on/off')
 @usb_device
-def write_read(usb_device):
-  usb_device.write_read([0x05, 0x07])
+def set(usb_device):
+  """
+  configure variables on the device
+  """
+  click.echo(usb_device.write_read([0x05, 0x07]))
 
 
+@cli.command()
+@click.option('--time', '-t', default='', help='Get the internal time')
+@click.option('--stim_on', '-p', default='', help='Get stimulation enable status')
+@click.option('--stim_port', '-sp', default='', help='Get the enabled stimulation port')
+@click.option('--tft_on', '-tft', default='', help='Get the tft status')
+@click.option('--broadcast_on', '-b', default='', help='Get MQTT broadcasting status')
+@click.option('--debug_on', '-b', default='', help='Get debug enable status')
+@click.option('--devices', 'd', default='', help='Get the connected devices')
+@usb_device
+def get(usb_device):
+  pass
+
+
+@cli.command()
+@usb_device
+def broadcast(usb_device):
+  """
+  Broadcast commands from one device to another over MQTT
+  """
+  pass
 
 
 
--- scripting/eOPRA_switcher_cli/src/utilities.py
+++ scripting/eOPRA_switcher_cli/src/utilities.py
@@ -35,33 +35,19 @@ class USBTalk:
     message.extend(data)
     message.append(self.comm_commands["stop"])
 
-    # for element in message:
-    #   self.serial_device.write(str(bytes(element)))
-    # print(type(message))
-
-    # self.serial_device.write(bytearray([1, 2, 3]))
-    print(bytearray(message))
-    print(message[1])
     self.serial_device.write(bytearray(message))
-    # self.serial_device.flush()
-
-    # self.serial_device.write(str(bytearray([1, 2, 3, 4])))
-
-    # print(message)
-    # return
 
     # receive the response
     try:
-      time.sleep(1)
-      print(self.serial_device.in_waiting)
-      output = self.serial_device.readline()
-      print(output)
-      print(list(output))
+      output = list(self.serial_device.readline())
     except:
       click.echo('Unable to communicate (send or receive) data to device.')
       return None
-    
+
     click.echo(output)
+
+    return output
+  
     
     return True
 
